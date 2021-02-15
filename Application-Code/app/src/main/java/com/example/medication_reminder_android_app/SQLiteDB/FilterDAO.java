b59ef7e93ebf58bd6de54bcee8a05dd786edbad5
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2021-02-15 11:00:56 -0600

    Moved project into Application-Code directory

---
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/SQLiteDB/FilterDAO.java
@@ -0,0 +1,15 @@
+package com.example.medication_reminder_android_app.SQLiteDB;
+
+/*
+Filter Data Access Object.
+This is the class for the Filter component to access the SQLite DB
+ */
+
+
+public class FilterDAO {
+
+    public FilterDAO(){
+
+    }
+
+}
---
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/UserInputHandler/CalenderEventCreator.java
@@ -0,0 +1,15 @@
+package com.example.medication_reminder_android_app.UserInputHandler;
+
+/*
+THis class handles sending medical appointment info to
+an external calender (either Google Calenders or the android calender)
+ */
+
+
+public class CalenderEventCreator {
+
+    public CalenderEventCreator(){
+
+    }
+
+}
