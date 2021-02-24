b6bd37fbb4dfd7d6a02334eee601a619b51c6e9e
Aliza Siddiqui <aliza.siddiqui24@gmail.com>
2021-02-24 12:01:44 -0600

    Edited ReminderRows Entity
    I have added the column names for the table, the constructor, as well as getter methods to get the private data

--- Application-Code/app/src/main/java/com/example/medication_reminder_android_app/SQLiteDB/RemindersTable.java
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/SQLiteDB/RemindersTable.java
@@ -5,7 +5,44 @@ import androidx.room.ColumnInfo;
 import androidx.room.PrimaryKey;
 
 @Entity
-public class RemindersTable {
-    @PrimaryKey
-    public int id;
+/**
+@author Aliza Siddiqui
+@lastModified 2/24/2021
+*/
+public class ReminderRows {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "Classification")
+    private String classification; // This categorizes the type of reminder (Medication or Appointment)
+
+    @ColumnInfo(name = "ApptTime")
+    private String time; //Format: HH:MM:SS
+
+    @ColumnInfo(name= "ApptDate")
+    private String date; //Format: YYYY-MM-DD
+
+    @ColumnInfo(autoGenerate = true)
+    private int med_appt_ident; // Will help Reminders Table connect with Medication Table (if Medication Reminder) and
+                                // Doctor Table (if Appointment Reminder)
+    
+    public ReminderRows(String Class, String Time, String Date){
+        this.classification = Class;
+        this.time = Time;
+        this.date = Date;
+
+    }
+
+    public String getClassification(){
+        return this.classification;
+    }
+
+    public int getTime(){
+        return this.time;
+    }
+
+    public int getDate(){
+        return this.date;
+    }
+
 }
