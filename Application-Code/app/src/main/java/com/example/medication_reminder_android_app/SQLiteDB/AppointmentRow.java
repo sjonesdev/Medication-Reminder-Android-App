5ba788057bd90652e444851c45c53196d1badb2f
Aliza Siddiqui <aliza.siddiqui24@gmail.com>
2021-02-27 15:32:09 -0600

    Appointment and ReminderRows
    Renamed ReminderTable to Reminder Rows
    Filled out the Appointment Entity

---
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/SQLiteDB/AppointmentRow.java
@@ -0,0 +1,60 @@
+package com.example.medication_reminder_android_app.SQLiteDB;
+
+import androidx.room.Entity;
+import androidx.room.ColumnInfo;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class AppointmentRow {
+    @PrimaryKey
+    public int id;
+
+    @ColumnInfo(name = "Appointment Location")
+    private String location; //Location of the appointment
+
+    @ColumnInfo(name = "Tags")
+    private String tags; //Categorizes the type of appointment
+
+    @ColumnInfo(name = "Notes")
+    private String notes; //Any notes that the user wants to add to the appointment information
+
+    @ColumnInfo(name = "TypeOfAppt")
+    private String typeOfAppt; //Classifies specifically what the appointment will consist of if not Doctor (i.e MRI, blood donation, etc.)
+    
+    @ColumnInfo(name = "RemindTableID")
+    private int remindTabID = new RemindersRow().getReminderTableID();
+
+    @ColumnInfo(name = "DoctorTableID")
+    private int docTabID = new DoctorTable().getDoctorTableID();
+
+    public AppointmentRow(String Location, String Tags, String Notes, String Type){
+       this.location = Location;
+       this.tags = Tags;
+       this.notes = Notes;
+       this.typeOfAppt = Type;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
+    public String getTags(){
+        return this.tags;
+    }
+
+    public String getNotes(){
+        return this.notes;
+    }
+
+    public String getTypeOfAppt(){
+        return this.typeOfAppt;
+    }
+
+    public int getApptTableID(){
+        return this.id;           //Returns the primary key of the specific Appointment entity
+    }
+
+    
+}
+
+
--- Application-Code/app/src/main/java/com/example/medication_reminder_android_app/SQLiteDB/RemindersRow.java
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/SQLiteDB/RemindersRow.java
@@ -9,7 +9,7 @@ import androidx.room.PrimaryKey;
 @author Aliza Siddiqui
 @lastModified 2/24/2021
 */
-public class ReminderRows {
+public class RemindersRow {
     @PrimaryKey(autoGenerate = true)
     private int id;
 
@@ -26,12 +26,13 @@ public class ReminderRows {
     private int med_appt_ident; // Will help Reminders Table connect with Medication Table (if Medication Reminder) and
                                 // Doctor Table (if Appointment Reminder)
     
-    public ReminderRows(String Class, String Time, String Date){
+    public RemindersRow(String Class, String Time, String Date){
         this.classification = Class;
         this.time = Time;
         this.date = Date;
 
     }
+
 
     public String getClassification(){
         return this.classification;
@@ -44,5 +45,10 @@ public class ReminderRows {
     public int getDate(){
         return this.date;
     }
+
+    public int getReminderTableID(){
+        return id;                      //Returns the primary key of the specific Reminder entity
+    }
+
 
 }
