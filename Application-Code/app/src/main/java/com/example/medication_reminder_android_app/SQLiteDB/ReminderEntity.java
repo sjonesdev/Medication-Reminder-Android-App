68d9ff7786b8973f66e294c75cf6133d96746328
Aliza Siddiqui <aliza.siddiqui24@gmail.com>
2021-02-27 16:18:11 -0600

    Created Reminder Entity
    This class will Replace RemindersRow to go with the naming convention for the other entities

---
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/SQLiteDB/ReminderEntity.java
@@ -0,0 +1,54 @@
+package com.example.medication_reminder_android_app.SQLiteDB;
+
+import androidx.room.Entity;
+import androidx.room.ColumnInfo;
+import androidx.room.PrimaryKey;
+
+@Entity
+/**
+@author Aliza Siddiqui
+@lastModified 2/27/2021
+*/
+public class ReminderEntity{
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "Classification")
+    private String classification; // This categorizes the type of reminder (Medication or Appointment)
+
+    @ColumnInfo(name = "ApptTime")
+    private String time; //Format: HH:MM:SS
+
+    @ColumnInfo(name= "ApptDate")
+    private String date; //Format: YYYY-MM-DD
+
+    @ColumnInfo(autoGenerate = true)
+    private int med_appt_ident; // Will help Reminders Table connect with Medication Table (if Medication Reminder) and
+                                // Doctor Table (if Appointment Reminder)
+    
+    public ReminderEntity(String Class, String Time, String Date){
+        this.classification = Class;
+        this.time = Time;
+        this.date = Date;
+
+    }
+
+
+    public String getClassification(){
+        return this.classification;
+    }
+
+    public int getTime(){
+        return this.time;
+    }
+
+    public int getDate(){
+        return this.date;
+    }
+
+    public int getReminderTableID(){
+        return id;                      //Returns the primary key of the specific Reminder entity
+    }
+
+
+}
