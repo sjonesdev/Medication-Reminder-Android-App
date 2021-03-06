965fc755cc3d620e0dcde0d6808efd90e60ce4ab
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2021-03-06 14:30:22 -0600

    Begun implementation of CalendarEventCreator

--- Application-Code/app/src/main/java/com/example/medication_reminder_android_app/UserInputHandler/AppointmentInputHandler.java
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/UserInputHandler/AppointmentInputHandler.java
@@ -3,10 +3,13 @@ package com.example.medication_reminder_android_app.UserInputHandler;
 //This class handles the appointments
 //Might need ot add an interface for adding info to Reminder table
 
+
+import java.util.Date;
 import java.util.Map;
 
 public class AppointmentInputHandler extends InputHandler {
 
+    CalenderEventCreator calenderEventCreator;
 
     /**
      *
@@ -16,6 +19,7 @@ public class AppointmentInputHandler extends InputHandler {
      */
     public AppointmentInputHandler() {
         super();
+        calenderEventCreator = new CalenderEventCreator();
     }
 
 
@@ -27,13 +31,23 @@ public class AppointmentInputHandler extends InputHandler {
      * @since 3-1-2021
      */
     void inputRequest(Map<String,String> info) {
-        String locationName = info.get("name");
+        String locationName = info.get("locationName");
         String locationAddress = info.get("address");
-        String apptType = info.get("type");
+        String apptName = info.get("name");
         String apptPurpose = info.get("purpose");
-        String doctor = info.get("doctor");
+        String doctorsStr = info.get("doctors");
+        String[] doctors = doctorsStr.split(",");
+        String date = info.get("date"); // format: YYYY-MM-DD
+        String time = info.get("time"); // HH:MM
         String tagsStr = info.get("tags");
         String[] tags = tagsStr.split(",");
+        boolean createCalendarEvent = Boolean.valueOf(info.get("createCalendarEvent"));
+        if(createCalendarEvent) {
+            String durationStr = info.get("apptDuration");
+            int duration = Integer.parseInt(durationStr);
+            calenderEventCreator.createCalendarEvent(apptName, locationName+", "+locationName,
+                    apptPurpose, doctors, date, time, duration); // name, location, description, people involved, date, time
+        }
 
         sendInput();
     }
