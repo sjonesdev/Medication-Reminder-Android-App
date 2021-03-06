5128ac901dab7e6c126fa0a9dda35034de925c50
Lucas <lucascthulu@gmail.com>
2021-03-06 15:16:51 -0600

    Item deletion xml file, need itemdeletionactivity class>

--- Application-Code/app/src/main/java/com/example/medication_reminder_android_app/UserInterface/InfoInput.java
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/UserInterface/InfoInput.java
@@ -4,7 +4,48 @@ package com.example.medication_reminder_android_app.UserInterface;
 
 public class InfoInput {
 
+    @XmlRootElement
+    public class Medication {
+        private String name;
+        private String dosage;
+        private String timeTaken;
+        private String[] days;
+        private Boolean recurring;
+        private String[] tags;
+
+        public Medication(String name, String dosage, String timeTaken, String[] days, Boolean recurring, String[] tags){
+            this.name = name;
+            this.dosage = dosage;
+            this.timeTaken = timeTaken;
+            this.days = days;
+            this.recurring = recurring;
+            this.tags = tags;
+        }
 
 
+    }
 
+
+    public class Doctor {
+        private String name;
+        private int phone;
+        private String address;
+
+        public Doctor(String name, int phone, String address){
+            this.name = name;
+            this.phone = phone;
+            this.address = address;
+        }
+    }
+
+    public class Appointment {
+        private String name;
+        private String address;
+
+        public Appointment(String name, String address){
+            this.name = name;
+            this.address = address;
+        }
+    }
 }
+ 
--- Application-Code/app/src/main/java/com/example/medication_reminder_android_app/UserInterface/ItemDeletion.java
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/UserInterface/ItemDeletion.java
@@ -1,8 +1,9 @@
 package com.example.medication_reminder_android_app.UserInterface;
-
+package com.example.medication_reminder_android_app.SQLiteDB;
 //This class handles deleting dr, med, appt info
 
-public class ItemDeletion {
+public class ItemDeletion
+{
 
 
 
