935bf23a83dc7503a4fc7272e6ec7aed33262bb9
Robert <rfahey2@lsu.edu>
2021-03-06 15:01:19 -0600

    Started work on MainActivity

---
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/InfoViewActivity.java
@@ -0,0 +1,42 @@
+package com.example.medication_reminder_android_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InfoViewActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info_view);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        //TODO
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        //TODO
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        //TODO
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        //TODO
+        super.onDestroy();
+    }
+}
---
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.medication_reminder_android_app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.viewInfoButton).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //setContentView(R.layout.info_view);
+                //startActivity(new Intent(MainActivity.this, InfoViewActivity.class));
+            }
+        });
+
+        findViewById(R.id.viewNotifsButton).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        findViewById(R.id.viewSettingsButton).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        //TODO
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        //TODO
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        //TODO
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        //TODO
+        super.onDestroy();
+    }
+}
