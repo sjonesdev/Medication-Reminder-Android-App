f197db1afb7316017ff99ca6e6cb8e0f8603de28
Aliza Siddiqui <aliza.siddiqui24@gmail.com>
2021-03-19 11:33:11 -0500

    Adding constructors in InApp and OutofApp to Fix Errors

--- Application-Code/app/src/main/java/com/example/medication_reminder_android_app/NotificationRelay/OutOfAppNotifications.java
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/NotificationRelay/OutOfAppNotifications.java
@@ -3,8 +3,12 @@ package com.example.medication_reminder_android_app.NotificationRelay;
 
 //This handles out of app notifications
 
+import com.example.medication_reminder_android_app.SQLiteDB.DatabaseRepository;
+
 public class OutOfAppNotifications extends Notifications{
 
-
+      public OutOfAppNotifications(DatabaseRepository repo){
+          super(repo);
+      }
 
 }
