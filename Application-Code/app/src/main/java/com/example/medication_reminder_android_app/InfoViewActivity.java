33bab10be9c3adfb8af17a7fcf73beca7cc412e1
Lucas <lucascthulu@gmail.com>
2021-03-22 11:53:02 -0500

    Fun new documentation!

--- Application-Code/app/src/main/java/com/example/medication_reminder_android_app/InfoViewActivity.java
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/InfoViewActivity.java
@@ -9,29 +9,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 public class InfoViewActivity extends AppCompatActivity implements InfoRecyclerAdapter.OnItemListener{
 
-    String names[];
-    String dosages[];
-    RecyclerView infoRecycler;
+    private String names[];
+    private String dosages[];
+    private RecyclerView infoRecycler;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        //set the layout to "info_view" when creating the activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_view);
 
+        //get the lists of all medication names and medication dosages
         names = getResources().getStringArray(R.array.debug_one);
-        dosages = getResources().getStringArray(R.array.debug_one);//This will likely be replaced with getting the info using Haley's methods
-        infoRecycler = findViewById(R.id.info_recycler);
+        dosages = getResources().getStringArray(R.array.debug_one);
 
+        /*
+        instantiate the recycler that will display the medications, instantiate its adapter,
+        and set the recycler's adapter and layout manager
+        */
+        infoRecycler = findViewById(R.id.info_recycler);
         InfoRecyclerAdapter infoadapter = new InfoRecyclerAdapter(this, names, dosages, this);
         infoRecycler.setAdapter(infoadapter);
         infoRecycler.setLayoutManager(new LinearLayoutManager(this));
-    }
 
-    @Override
-    public void onStart() {
-        super.onStart();
+        /*
+        When the "Back" button is clicked, end the InfoViewActivity and return to the MainActivity
+         */
         findViewById(R.id.info_back_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,6 +51,12 @@ public class InfoViewActivity extends AppCompatActivity implements InfoRecyclerA
                 startActivity(new Intent(InfoViewActivity.this, InfoInputActivity.class));
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
     }
 
     @Override
--- Application-Code/app/src/main/java/com/example/medication_reminder_android_app/MainActivity.java
+++ Application-Code/app/src/main/java/com/example/medication_reminder_android_app/MainActivity.java
@@ -1,31 +1,46 @@
 package com.example.medication_reminder_android_app;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    @author Robert
+    When the activity is created on app startup, set the current layout being displayed to
+    "activity_main" and set event listeners for the three buttons displayed onscreen
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+        @author Robert
+        When the "View My Info button is clicked", swap activities from MainActivity to InfoViewActivity
+         */
         findViewById(R.id.viewInfoButton).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, InfoViewActivity.class));
             }
         });
 
+        /**
+        @author Robert
+         When the "Notifications" button is clicked, swap activities from MainActivity to NotificationActivity
+         */
         findViewById(R.id.viewNotifsButton).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, NotificationActivity.class));
             }
         });
 
+        /**
+        @author Robert
+        When the "Settings" button is clicked, swap activities from MainActivity to SettingsActivity
+         */
         findViewById(R.id.viewSettingsButton).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, SettingsActivity.class));
